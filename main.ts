c6650b04d12f1cd29aa3e98ac87e64d73b4e3744
tonyspm <73495998+tonyspm@users.noreply.github.com>
2020-11-23 14:36:54 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,21 @@
 let T1 = 0
+let T2 = 0
 let T3 = 0
 let Temperatura = 0
-function TProm (núm: number) {
-    let T2 = 0
+function TProm () {
     T1 = input.temperature()
     basic.pause(1000)
-    T3 = input.temperature()
+    T2 = input.temperature()
     basic.pause(1000)
     T3 = input.temperature()
-    basic.showNumber(T1 + (T2 + T3 / 3))
+    return (T1 + (T2 + T3)) / 3
 }
 basic.forever(function () {
-    Temperatura = 26
+    Temperatura = TProm()
+    basic.pause(2000)
     if (Temperatura >= 25) {
         basic.showIcon(IconNames.Happy)
+        basic.pause(2000)
     } else {
         basic.showIcon(IconNames.Sad)
         basic.pause(2000)
